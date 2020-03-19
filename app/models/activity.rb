72434859c5a8811937bd94da6952189743e3f047
Max Wolff <maxwolff1103@gmail.com>
2020-03-19 12:36:19 +0100

    Model Validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,6 @@
 class Activity < ApplicationRecord
+  validates :name, presence: true
+  validates :link, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
