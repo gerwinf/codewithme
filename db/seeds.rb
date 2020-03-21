9242252b12c3db8b217fb901698094e594741942
Max Wolff <maxwolff1103@gmail.com>
2020-03-21 10:53:26 +0100

    Dateetime now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # Activity Seed File assigining 5 activities to latest user
 
-activity_1 = Activity.create(name: "Javascript 30 Day Challenge", link: "", start_time: , end_time: )
-activity_2 = Activity.create(name: "Discuss Intro to Python Course", link: "", start_time: , end_time: )
-activity_3 = Activity.create(name: "Watch Rick and Morty + Beer", link: "", start_time: , end_time: )
-activity_4 = Activity.create(name: "Complete Kitt Optionals - Week 1-3", link: "", start_time: , end_time: )
-activity_5 = Activity.create(name: "Fifa 2020 Tournament", link: "", start_time: , end_time: )
+activity_1 = Activity.create(name: "Javascript 30 Day Challenge", link: "", start_time: DateTime.now, end_time: DateTime.now)
+activity_2 = Activity.create(name: "Discuss Intro to Python Course", link: "", start_time: DateTime.now, end_time: DateTime.now)
+activity_3 = Activity.create(name: "Watch Rick and Morty + Beer", link: "", start_time: DateTime.now, end_time: DateTime.now)
+activity_4 = Activity.create(name: "Complete Kitt Optionals - Week 1-3", link: "", start_time: DateTime.now, end_time: DateTime.now)
+activity_5 = Activity.create(name: "Fifa 2020 Tournament", link: "", start_time: DateTime.now, end_time: DateTime.now)
 
 activities = []
 
